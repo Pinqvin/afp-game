4279eb98adf6691cc5e5c0a84ee78c9b6a8ec187
Touko Keski-Sämpi <ankkamiess@gmail.com>
2013-10-09 16:07:12 +0300

    Added Box2D to project.

--- src/world.cpp
+++ src/world.cpp
@@ -6,99 +6,111 @@
 
 /// Constructor
 AFP::World::World(sf::RenderWindow& window):
-    mWindow(window), mWorldView(window.getDefaultView()),
-    mTextures(), mSceneGraph(), mSceneLayers(),
-    mWorldBounds(0.f, 0.f, mWorldView.getSize().x, 2000.f),
-    mSpawnPosition(mWorldView.getSize().x / 2.f, mWorldBounds.height - mWorldView.getSize().y / 2.f),
-    mScrollSpeed(-50.f), mPlayer(nullptr)
+	mWindow(window), mWorldView(window.getDefaultView()),
+	mTextures(), mSceneGraph(), mSceneLayers(),
+	mWorldBounds(0.f, 0.f, mWorldView.getSize().x, 2000.f),
+	mSpawnPosition(mWorldView.getSize().x / 2.f, mWorldBounds.height - mWorldView.getSize().y / 2.f),
+	mScrollSpeed(-50.f), mPlayer(nullptr)
 {
-    loadTextures();
-    buildScene();
+	loadTextures();
+	buildScene();
 
-    mWorldView.setCenter(mSpawnPosition);
+	mWorldView.setCenter(mSpawnPosition);
+
+	/// Box2D
+	createWorld();
 
 }
 
 /// Load all the textures required for the world
 void AFP::World::loadTextures()
 {
-    mTextures.load(Textures::Player, "Media/Textures/Eagle.png");
-    mTextures.load(Textures::Desert, "Media/Textures/Desert.png");
+	mTextures.load(Textures::Player, "Media/Textures/Eagle.png");
+	mTextures.load(Textures::Desert, "Media/Textures/Desert.png");
 
 }
 
 /// Build the scene depicted by the world
 void AFP::World::buildScene()
 {
-    /// Initialize the different scene layers
-    for (std::size_t i = 0; i < LayerCount; ++i)
-    {
-        SceneNode::Ptr layer(new SceneNode());
-        mSceneLayers[i] = layer.get();
+	/// Initialize the different scene layers
+	for (std::size_t i = 0; i < LayerCount; ++i)
+	{
+		SceneNode::Ptr layer(new SceneNode());
+		mSceneLayers[i] = layer.get();
 
-        mSceneGraph.attachChild(std::move(layer));
+		mSceneGraph.attachChild(std::move(layer));
 
-    }
+	}
 
-    /// Set the background texture to be tiled
-    sf::Texture& texture = mTextures.get(Textures::Desert);
-    sf::IntRect textureRect(mWorldBounds);
-    texture.setRepeated(true);
+	/// Set the background texture to be tiled
+	sf::Texture& texture = mTextures.get(Textures::Desert);
+	sf::IntRect textureRect(mWorldBounds);
+	texture.setRepeated(true);
 
-    /// Make the background sprite as big as the whole world
-    std::unique_ptr<SpriteNode> backgroundSprite(
-            new SpriteNode(texture, textureRect));
+	/// Make the background sprite as big as the whole world
+	std::unique_ptr<SpriteNode> backgroundSprite(
+		new SpriteNode(texture, textureRect));
 
-    backgroundSprite->setPosition(
-            mWorldBounds.left,
-            mWorldBounds.top);
-    mSceneLayers[Background]->attachChild(std::move(backgroundSprite));
+	backgroundSprite->setPosition(
+		mWorldBounds.left,
+		mWorldBounds.top);
+	mSceneLayers[Background]->attachChild(std::move(backgroundSprite));
 
-    /// Set the player to the world
-    std::unique_ptr<Player> leader(new Player(mTextures));
-    mPlayer = leader.get();
+	/// Set the player to the world
+	std::unique_ptr<Player> leader(new Player(mTextures));
+	mPlayer = leader.get();
 
-    mPlayer->setPosition(mSpawnPosition);
-    mPlayer->setVelocity(40.f, mScrollSpeed);
-    mSceneLayers[Foreground]->attachChild(std::move(leader));
+	mPlayer->setPosition(mSpawnPosition);
+	mPlayer->setVelocity(40.f, mScrollSpeed);
+	mSceneLayers[Foreground]->attachChild(std::move(leader));
 
-    /// Add some new planes to follow the player plane
-    std::unique_ptr<Player> leftEscort(new Player(mTextures));
-    /// Position is relative to the leader (player)
-    leftEscort->setPosition(-80.f, 50.f);
-    mPlayer->attachChild(std::move(leftEscort));
+	/// Add some new planes to follow the player plane
+	std::unique_ptr<Player> leftEscort(new Player(mTextures));
+	/// Position is relative to the leader (player)
+	leftEscort->setPosition(-80.f, 50.f);
+	mPlayer->attachChild(std::move(leftEscort));
 
-    std::unique_ptr<Player> rightEscort(new Player(mTextures));
-    rightEscort->setPosition(80.f, 50.f);
-    mPlayer->attachChild(std::move(rightEscort));
-    
+	std::unique_ptr<Player> rightEscort(new Player(mTextures));
+	rightEscort->setPosition(80.f, 50.f);
+	mPlayer->attachChild(std::move(rightEscort));
+
+}
+
+void AFP::World::createWorld() 
+{
+	b2Vec2 gravity(0.0f, -9.81f); 
+	mWorldBox = new b2World(gravity);
+
+	mBodyDef.position.Set(0.0f, -10.0f);
+	mGroundBody = mWorldBox->CreateBody(&mBodyDef);	mGroundBox.SetAsBox(50.0f, 10.0f);	mGroundBody->CreateFixture(&mGroundBox, 0.0f);
 }
 
 /// Draw the scene to the window
 void AFP::World::draw()
 {
-    mWindow.setView(mWorldView);
-    mWindow.draw(mSceneGraph);
+	mWindow.setView(mWorldView);
+	mWindow.draw(mSceneGraph);
 
 }
 
 /// Update the world
 void AFP::World::update(sf::Time dt)
 {
-    mWorldView.move(0.f, mScrollSpeed * dt.asSeconds());
+	mWorldView.move(0.f, mScrollSpeed * dt.asSeconds());
 
-    sf::Vector2f position = mPlayer->getPosition();
-    sf::Vector2f velocity = mPlayer->getVelocity();
+	sf::Vector2f position = mPlayer->getPosition();
+	sf::Vector2f velocity = mPlayer->getVelocity();
 
-    if (position.x <= mWorldBounds.left + 150
-            || position.x >= mWorldBounds.left + mWorldBounds.width - 150)
-    {
-        velocity.x = -velocity.x;
-        mPlayer->setVelocity(velocity);
+	if (position.x <= mWorldBounds.left + 150
+		|| position.x >= mWorldBounds.left + mWorldBounds.width - 150)
+	{
+		velocity.x = -velocity.x;
+		mPlayer->setVelocity(velocity);
 
-    }
+	}
 
-    mSceneGraph.update(dt);
+	mSceneGraph.update(dt);
 
 }
 
--- src/world.hpp
+++ src/world.hpp
@@ -13,107 +13,133 @@
 #include <SFML/Graphics/View.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/NonCopyable.hpp>
+#include <Box2D/Box2D.h>
 #include <array>
 
 /// Forward declaration
 namespace sf
 {
-    class RenderWindow;
+	class RenderWindow;
 
 }
 
 namespace AFP
 {
-    class World : private sf::NonCopyable
-    {
-        public:
-            /// Constructor
-            ///
-            ///
-            explicit World(sf::RenderWindow& window);
+	class World : private sf::NonCopyable
+	{
+	public:
+		/// Constructor
+		///
+		///
+		explicit World(sf::RenderWindow& window);
 
-            /// Update the SceneGraph
-            ///
-            /// Updates the scenegraph
-            void update(sf::Time dt);
+		/// Update the SceneGraph
+		///
+		/// Updates the scenegraph
+		void update(sf::Time dt);
 
-            /// Draws the scenegraph
-            ///
-            /// Draws all the sprites inside the scenegraph
-            void draw();
+		/// Draws the scenegraph
+		///
+		/// Draws all the sprites inside the scenegraph
+		void draw();
 
-        private:
-            /// Loads all the textures required for the world
-            ///
-            /// Textures are stored in the mTextures TextureHolder
-            void loadTextures();
+	private:
+		/// Loads all the textures required for the world
+		///
+		/// Textures are stored in the mTextures TextureHolder
+		void loadTextures();
 
-            /// Builds the scene for the world
-            ///
-            /// Builds the scene using the textures loaded in the
-            /// TextureHolder.
-            void buildScene();
+		/// Builds the scene for the world
+		///
+		/// Builds the scene using the textures loaded in the
+		/// TextureHolder.
+		void buildScene();
 
-            /// Divite the draws to two different categories: background
-            /// (the area the player can't interract with) and foreground
-            ///
-            /// LayerCount isn't used to differentiate layers, it's used to
-            /// keep a count of the different layers.
-            enum Layer
-            {
-                Background,
-                Foreground,
-                LayerCount
-            };
+		/// Create world with gravity
+		///
+		/// Build the Box2D world.
+		void createWorld();
 
-            /// Reference to the render window
-            ///
-            /// Used to draw all the scene nodes
-            sf::RenderWindow& mWindow;
+		/// Divite the draws to two different categories: background
+		/// (the area the player can't interract with) and foreground
+		///
+		/// LayerCount isn't used to differentiate layers, it's used to
+		/// keep a count of the different layers.
+		enum Layer
+		{
+			Background,
+			Foreground,
+			LayerCount
+		};
 
-            /// The view the player has on the world
-            ///
-            /// Used to determine which part of the world
-            /// the player can see.
-            sf::View mWorldView;
+		/// Reference to the render window
+		///
+		/// Used to draw all the scene nodes
+		sf::RenderWindow& mWindow;
 
-            /// Holds all the textures for the world
-            ///
-            /// Textures are loaded during loadTextures() call.
-            TextureHolder mTextures;
+		/// The view the player has on the world
+		///
+		/// Used to determine which part of the world
+		/// the player can see.
+		sf::View mWorldView;
 
-            /// Scene graph holds all the entities together
-            ///
-            ///
-            SceneNode mSceneGraph;
+		/// Holds all the textures for the world
+		///
+		/// Textures are loaded during loadTextures() call.
+		TextureHolder mTextures;
 
-            /// Differentiate scene nodes by their layer
-            ///
-            ///
-            std::array<SceneNode*, LayerCount> mSceneLayers;
+		/// Scene graph holds all the entities together
+		///
+		///
+		SceneNode mSceneGraph;
 
-            /// Defines the world bounds
-            ///
-            /// Player can't go out of bounds so we need to define the
-            /// limits.
-            sf::FloatRect mWorldBounds;
-            
-            /// Defines the player's spawn position in the world
-            ///
-            /// 
-            sf::Vector2f mSpawnPosition;
+		/// Differentiate scene nodes by their layer
+		///
+		///
+		std::array<SceneNode*, LayerCount> mSceneLayers;
 
-            /// Defines the speed the world scrolls at
-            ///
-            /// Most likely set to the same as the player's speed
-            float mScrollSpeed;
+		/// Defines the world bounds
+		///
+		/// Player can't go out of bounds so we need to define the
+		/// limits.
+		sf::FloatRect mWorldBounds;
 
-            /// Pointer to the player entity
-            ///
-            ///
-            Player* mPlayer;
+		/// Defines the player's spawn position in the world
+		///
+		/// 
+		sf::Vector2f mSpawnPosition;
 
-    };
+		/// Defines the speed the world scrolls at
+		///
+		/// Most likely set to the same as the player's speed
+		float mScrollSpeed;
+
+		/// Pointer to the player entity
+		///
+		///
+		Player* mPlayer;
+
+		/// Za Warudo
+		///
+		/// Box2D world.
+		b2World* mWorldBox;
+
+		/// Ground body definition
+		///
+		///
+		b2BodyDef mBodyDef;
+
+		/// Ground body
+		///
+		///
+		b2Body* mGroundBody;
+
+		/// Ground polygon
+		///
+		///
+		b2PolygonShape mGroundBox;
+
+	};
 
 }
 
