6753ed861ef512b1160edb80ae0dbe676aa6bd1b
Touko Keski-Sämpi <ankkamiess@gmail.com>
2013-10-09 16:27:30 +0300

    Fixed indentation in world files.

--- src/world.hpp
+++ src/world.hpp
@@ -19,127 +19,127 @@
 /// Forward declaration
 namespace sf
 {
-	class RenderWindow;
+    class RenderWindow;
 
 }
 
 namespace AFP
 {
-	class World : private sf::NonCopyable
-	{
-	public:
-		/// Constructor
-		///
-		///
-		explicit World(sf::RenderWindow& window);
+    class World : private sf::NonCopyable
+    {
+    public:
+        /// Constructor
+        ///
+        ///
+        explicit World(sf::RenderWindow& window);
 
-		/// Update the SceneGraph
-		///
-		/// Updates the scenegraph
-		void update(sf::Time dt);
+        /// Update the SceneGraph
+        ///
+        /// Updates the scenegraph
+        void update(sf::Time dt);
 
-		/// Draws the scenegraph
-		///
-		/// Draws all the sprites inside the scenegraph
-		void draw();
+        /// Draws the scenegraph
+        ///
+        /// Draws all the sprites inside the scenegraph
+        void draw();
 
-	private:
-		/// Loads all the textures required for the world
-		///
-		/// Textures are stored in the mTextures TextureHolder
-		void loadTextures();
+    private:
+        /// Loads all the textures required for the world
+        ///
+        /// Textures are stored in the mTextures TextureHolder
+        void loadTextures();
 
-		/// Builds the scene for the world
-		///
-		/// Builds the scene using the textures loaded in the
-		/// TextureHolder.
-		void buildScene();
+        /// Builds the scene for the world
+        ///
+        /// Builds the scene using the textures loaded in the
+        /// TextureHolder.
+        void buildScene();
 
-		/// Create world with gravity
-		///
-		/// Build the Box2D world.
-		void createWorld();
+        /// Create world with gravity
+        ///
+        /// Build the Box2D world.
+        void createWorld();
 
-		/// Divite the draws to two different categories: background
-		/// (the area the player can't interract with) and foreground
-		///
-		/// LayerCount isn't used to differentiate layers, it's used to
-		/// keep a count of the different layers.
-		enum Layer
-		{
-			Background,
-			Foreground,
-			LayerCount
-		};
+        /// Divite the draws to two different categories: background
+        /// (the area the player can't interract with) and foreground
+        ///
+        /// LayerCount isn't used to differentiate layers, it's used to
+        /// keep a count of the different layers.
+        enum Layer
+        {
+            Background,
+            Foreground,
+            LayerCount
+        };
 
-		/// Reference to the render window
-		///
-		/// Used to draw all the scene nodes
-		sf::RenderWindow& mWindow;
+        /// Reference to the render window
+        ///
+        /// Used to draw all the scene nodes
+        sf::RenderWindow& mWindow;
 
-		/// The view the player has on the world
-		///
-		/// Used to determine which part of the world
-		/// the player can see.
-		sf::View mWorldView;
+        /// The view the player has on the world
+        ///
+        /// Used to determine which part of the world
+        /// the player can see.
+        sf::View mWorldView;
 
-		/// Holds all the textures for the world
-		///
-		/// Textures are loaded during loadTextures() call.
-		TextureHolder mTextures;
+        /// Holds all the textures for the world
+        ///
+        /// Textures are loaded during loadTextures() call.
+        TextureHolder mTextures;
 
-		/// Scene graph holds all the entities together
-		///
-		///
-		SceneNode mSceneGraph;
+        /// Scene graph holds all the entities together
+        ///
+        ///
+        SceneNode mSceneGraph;
 
-		/// Differentiate scene nodes by their layer
-		///
-		///
-		std::array<SceneNode*, LayerCount> mSceneLayers;
+        /// Differentiate scene nodes by their layer
+        ///
+        ///
+        std::array<SceneNode*, LayerCount> mSceneLayers;
 
-		/// Defines the world bounds
-		///
-		/// Player can't go out of bounds so we need to define the
-		/// limits.
-		sf::FloatRect mWorldBounds;
+        /// Defines the world bounds
+        ///
+        /// Player can't go out of bounds so we need to define the
+        /// limits.
+        sf::FloatRect mWorldBounds;
 
-		/// Defines the player's spawn position in the world
-		///
-		/// 
-		sf::Vector2f mSpawnPosition;
+        /// Defines the player's spawn position in the world
+        ///
+        /// 
+        sf::Vector2f mSpawnPosition;
 
-		/// Defines the speed the world scrolls at
-		///
-		/// Most likely set to the same as the player's speed
-		float mScrollSpeed;
+        /// Defines the speed the world scrolls at
+        ///
+        /// Most likely set to the same as the player's speed
+        float mScrollSpeed;
 
-		/// Pointer to the player entity
-		///
-		///
-		Player* mPlayer;
+        /// Pointer to the player entity
+        ///
+        ///
+        Player* mPlayer;
 
-		/// Za Warudo
-		///
-		/// Box2D world.
-		b2World* mWorldBox;
+        /// Za Warudo
+        ///
+        /// Box2D world.
+        b2World* mWorldBox;
 
-		/// Ground body definition
-		///
-		///
-		b2BodyDef mBodyDef;
+        /// Ground body definition
+        ///
+        ///
+        b2BodyDef mBodyDef;
 
-		/// Ground body
-		///
-		///
-		b2Body* mGroundBody;
+        /// Ground body
+        ///
+        ///
+        b2Body* mGroundBody;
 
-		/// Ground polygon
-		///
-		///
-		b2PolygonShape mGroundBox;
+        /// Ground polygon
+        ///
+        ///
+        b2PolygonShape mGroundBox;
 
-	};
+    };
 
 }
 
